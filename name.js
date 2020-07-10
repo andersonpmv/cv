5300bda56b790656e8ac101e96d8984c0ed4df7f
andersonpmv <andersonpmv@gmail.com>
2020-07-09 21:37:52 -0300

    name

---
+++ name.js
@@ -0,0 +1,11 @@
+let rs = require('readline-sync');
+
+let name = rs.question('Digite seu nome: ');
+
+let cont = 0;
+let fim = 3
+
+for(let cont = 0; cont < fim; cont ++){
+    console.log(name);
+}
+
