8a1382272a47ef011087ed3b9e785cde51555c09
andersonpmv <andersonpmv@gmail.com>
2020-07-09 21:46:49 -0300

    alterei de 03 para 05

--- name.js
+++ name.js
@@ -3,7 +3,7 @@ let rs = require('readline-sync');
 let name = rs.question('Digite seu nome: ');
 
 let cont = 0;
-let fim = 3
+let fim = 5
 
 for(let cont = 0; cont < fim; cont ++){
     console.log(name);
